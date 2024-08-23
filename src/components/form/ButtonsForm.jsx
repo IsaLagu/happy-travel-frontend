daa49ff0bbc29577ab1283511e1f5baf45971e77
Rebeca Garcia <rebe.ga87@gmail.com>
2024-08-23 11:52:28 +0200

    add useCrud hook & buttonsForm logic

--- src/components/form/ButtonsForm.jsx
+++ src/components/form/ButtonsForm.jsx
@@ -1,14 +1,41 @@
 import Button from "../general/Button";
+import PropTypes from "prop-types";
+import useCrud from "../../hooks/useCrud";
+import react from "@vitejs/plugin-react-swc";
 
-const ButtonsForm = () => {
+
+const ButtonsForm = ({ operationType, data }) => {
+    const { triggerOperation, loading, error } = useCrud(operationType, data);
+
+    const accept = () => {
+        triggerOperation()
+    }
+
+    const cancel = () => {
+        alert("Operación cancelada")
+    }
+
+    react.useEffect(() => {
+        if (!loading && error) {
+            alert(error)
+        } else if (!loading) {
+            alert("Operación exitosa")
+        }
+    }, [loading, error])
+
     return (
         <div className="flex flex-wrap gap-4">
-            <Button onClick="" buttonStyle="bg-green" buttonText="Aceptar"/>
-            <Button buttonStyle="bg-red" buttonText="Cancelar"/>
-            
+            <Button onClick={accept} buttonStyle="bg-green" buttonText="Aceptar" />
+            <Button onClick={cancel} buttonStyle="bg-red" buttonText="Cancelar" />
+
 
         </div>
     )
+};
+
+ButtonsForm.propTypes = {
+    operationType: PropTypes.string,
+    data: PropTypes.string,
 };
 
 export default ButtonsForm
