03bc47305f8a5591b25a46af632e7298159a1c6c
Rebeca Garcia <rebe.ga87@gmail.com>
2024-08-23 16:00:02 +0200

    fixing home styles

--- src/components/home/Pagination.jsx
+++ src/components/home/Pagination.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 const Pagination = ({ totalPages }) => {
-    const [currentPage, setCurrentPage] = useState (1);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const previousPage = () => {
-        if (currentPage > 1 ) {
+        if (currentPage > 1) {
             setCurrentPage(currentPage - 1)
         }
     };
-    
+
     const nextPage = () => {
         if (currentPage < totalPages) {
             setCurrentPage(currentPage + 1)
@@ -16,23 +16,26 @@ const Pagination = ({ totalPages }) => {
     };
 
     return (
-        <div className="flex items-center justify-center gap-4">
-            <button 
-            onClick={previousPage}
-            className="h-[40px] w-[60px] pb-8 items-center rounded-full bg-blue text-white text-3xl"
-            disabled= {currentPage ===1}>
-                ⪡
-            </button>
-            <span className="font-jaldi text-blue font-bold text-[25px]">
-                {currentPage}
-            </span>
-            <button 
-            onClick={nextPage}
-            className="h-[40px] w-[60px] pb-8 items-center rounded-full bg-blue text-white text-3xl"
->
-                ⪢
-            </button>
+        <div className="bg-white flex justify-center w-full mt-5">
+            <div className="sticky bottom-0 flex items-center justify-center gap-4 mt-4 mb-10">
+                <button
+                    onClick={previousPage}
+                    className="h-[40px] w-[60px] pb-8 items-center rounded-full bg-blue text-white text-3xl"
+                    disabled={currentPage === 1}>
+                    ⪡
+                </button>
+                <span className="font-jaldi text-blue font-bold text-[25px]">
+                    {currentPage}
+                </span>
+                <button
+                    onClick={nextPage}
+                    className="h-[40px] w-[60px] pb-8 items-center rounded-full bg-blue text-white text-3xl"
+                >
+                    ⪢
+                </button>
+            </div>
         </div>
+
     )
 };
 
