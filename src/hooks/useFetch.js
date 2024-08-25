2dc08bcccc4fc4fc1fafde83cb815201bdbc9a50
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-08-25 19:46:41 +0100

    add usePost

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,32 +1,33 @@
 import { useState, useEffect } from "react";
 import { API_URL } from "../config";
 
-const useFetch = (endpoint, options) => {
+const useFetch = (endpoint, options, shouldFetch = true) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(`${API_URL}${endpoint}`, options);
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-        const result = await response.json();
-        setData(result);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${API_URL}${endpoint}`, options);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
       }
-    };
+      const result = await response.json();
+      setData(result);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    if (!shouldFetch) return;
     fetchData();
-  }, [endpoint, options]);
+  }, [endpoint]);
 
-  return { data, loading, error };
+  return { data, loading, error, fetch: fetchData };
 };
 
 export default useFetch;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Input from "../components/general/Input";
-import ButtonsForm from "../components/form/ButtonsForm";
+import Button from "../components/general/Button";
+import usePost from "../hooks/usePost";
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { error, executePost, data } = usePost("/auth/register");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const user = { name, email, password };
     console.log(user);
+    executePost({ name, email, password });
   };
+
+  useEffect(() => {
+    console.log("Data", data);
+  }, [data]);
 
   return (
     <div className="flex justify-center mt-12">
       <div className="w-[370px] h-[487px] px-[35px] bg-white border-4 border-cream rounded-2xl">
-        <form action="#">
+        <form>
           <h5 className="text-2xl text-center pt-[10px] font-bold text-red border-red border-b-2 mb-[22px] pb-2">
             Registro de usuario
           </h5>
@@ -58,7 +67,15 @@ const SignUp = () => {
                 placeholder="Escribe tu contaseña"
               />
             </div>
-            <ButtonsForm />
+            {error && (
+              <div class="p-1 m-[-25px] text-sm text-center text-red rounded-lg" role="alert">
+                <span class="font-medium">There was an error:</span> {error}
+              </div>
+            )}
+            <div className="flex flex-wrap gap-4 justify-center">
+              <Button onClick={handleSubmit} buttonStyle="bg-green" buttonText="Aceptar" />
+              <Button onClick={() => undefined} buttonStyle="bg-red" buttonText="Cancelar" />
+            </div>
             <div className="text-xl font-semibold text-blue text-center mt-[-20px]">
               ¿Ya tienes cuenta? Accede {""}
               <a href="#" className="text-green font-semibold hover:underline">
