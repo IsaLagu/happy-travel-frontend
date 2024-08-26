198b5acbadeb5069100bdee6dbd6ab17dc10003a
Carol <calonsogon6@gmail.com>
2024-08-26 09:02:10 +0200

    Validation ok

--- src/components/general/Input.jsx
+++ src/components/general/Input.jsx
@@ -1,23 +1,3 @@
-/* import React from "react";
-
-const Input = ({ value, onChange, type = "text", name, id, className, placeholder, required }) => {
-  return (
-    <input
-      value={value}
-      onChange={onChange}
-      type={type}
-      name={name}
-      id={id}
-      className={`h-10 font-light text-xl text-blue bg-cream shadow-inner shadow-slate-400 rounded-full border-gray-300 placeholder-blue block pl-[17px] py-2.5${
-        className ? " " + className : ""
-      }`}
-      placeholder={placeholder}
-      required
-    />
-  );
-};
-
-export default Input; */
 import React from "react";
 
 const Input = ({ type = "text", id, className, placeholder, ...rest }) => {
