43282e84437897c2b125e1b79cba28434236196a
Rebeca Garcia <rebe.ga87@gmail.com>
2024-08-28 09:39:57 +0200

    DeletAlert buttons logic

--- src/components/alerts/DeleteAlert.jsx
+++ src/components/alerts/DeleteAlert.jsx
@@ -1,13 +1,16 @@
 import ButtonsForm from "../form/ButtonsForm";
 
-const DeleteAlert = () => {
+const DeleteAlert = ({onCancel, onConfirm}) => {
     return (
         <div className="flex justify-center items-center mt-12 p-24">
             <div className="w-[500px] h-[200px] bg-cream rounded-2xl p-10 flex flex-col items-center">
                     <h5 className="text-[25px] text-center pt-[10px] font-jaldi text-blue mb-[10px] pb-2">
                         ¿Quieres eliminar este destino?
                     </h5>
-                    <ButtonsForm/>
+                    <ButtonsForm
+                        onSubmit={onConfirm}
+                        onCancel={onCancel}
+                    />
             </div>
         </div>
     );
